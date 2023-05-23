488bf8d4d9524315f5e7a6cfefebbd9daf8d2273
Russell Jones <justyrones@gmail.com>
2023-05-23 16:15:28 -0600

    the copy

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import React from "react";
+import './App.css';
+import Eightball from './Eightball.js'
+
+function App() {
+  return (
+    <div style={{paddingTop: "150px"}}>
+        <Eightball />
+    </div>
+  );
+}
+
+export default App;
---
+++ src/Eightball.js
@@ -0,0 +1,74 @@
+import React, { useState } from "react";
+import eightball from "./eightball.css";
+//testing
+const ballInfo = [
+  { msg: "It is certain.", color: "green" },
+  { msg: "It is decidedly so.", color: "green" },
+  { msg: "Without a doubt.", color: "green" },
+  { msg: "Yes - definitely.", color: "green" },
+  { msg: "You may rely on it.", color: "green" },
+  { msg: "As I see it, yes.", color: "green" },
+  { msg: "Most likely.", color: "green" },
+  { msg: "Outlook good.", color: "green" },
+  { msg: "Yes.", color: "green" },
+  { msg: "Signs point to yes.", color: "goldenrod" },
+  { msg: "Reply hazy, try again.", color: "goldenrod" },
+  { msg: "Ask again later.", color: "goldenrod" },
+  { msg: "Better not tell you now.", color: "goldenrod" },
+  { msg: "Cannot predict now.", color: "goldenrod" },
+  { msg: "Concentrate and ask again.", color: "goldenrod" },
+  { msg: "Don't count on it.", color: "red" },
+  { msg: "My reply is no.", color: "red" },
+  { msg: "My sources say no.", color: "red" },
+  { msg: "Outlook not so good.", color: "red" },
+  { msg: "Very doubtful.", color: "red" },
+];
+/**App calls Eightball component. Receives key of answers that is array of objects. Uses State
+ * to dynamically display an eightball with changing background colors
+ * and messages. Returns that 8ball component
+ */
+function Eightball({ answers = ballInfo }) {
+  const defaultState = {
+    msg: "Think of a Question",
+    color: "black",
+  };
+
+  const colorCounts = { green: 0, goldenrod: 0, red: 0 };
+  const [ball, setBall] = useState(defaultState);
+  const [colorCount, setColorCounts] = useState(colorCounts);
+
+  function clickEightball() {
+    const index = Math.floor(Math.random() * answers.length);
+    const answer = answers[index];
+    const color = answer.color;
+
+    setBall(answer);
+
+    setColorCounts(prev=>{
+      return {...prev, color: prev[color]++};
+    })
+  }
+
+  function resetEightBall() {
+    setBall(defaultState);
+  }
+
+  return (
+    <div className="eightball-container">
+      <div
+        className="eightball"
+        style={{ backgroundColor: ball.color, color: "white" }}
+        onClick={clickEightball}
+
+      >
+        <h1>{ball.msg}</h1>
+      </div>
+      <button onClick={resetEightBall}>Reset</button>
+      <div>green count is: {colorCount.green}</div>
+      <div>goldenrod count is: {colorCount.goldenrod}</div>
+      <div>red count is: {colorCount.red}</div>
+    </div>
+  );
+}
+
+export default Eightball;
